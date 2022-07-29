f95704cbc0bcf1b54a086f4ace91a0e8b369bf71
milessteamaccount <ronaldlamdev@gmail.com>
2022-07-28 23:54:58 -0500

    functional mobile nav menu

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../images/qc2020logo-mini-new.png';
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
 
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+
   return (
     <div className='max-w-[1260px] w-full max-h-[100px] flex justify-between items-center mx-auto mt-5 px-2 bg-black/30 rounded-xl border border-gray-100 shadow-lg'>
 
@@ -24,23 +27,21 @@ const Navbar = () => {
 
       {/* Navbar links for tablets and mobile */}
       <div>
-        <AiOutlineMenu className='md:hidden text-white/90 cursor-pointer' size={25} />
+        <AiOutlineMenu className='md:hidden text-white/90 cursor-pointer' size={25} onClick={() => setNav(!nav)}/>
       </div>
+        {/* Overlay */}
+        <div className={nav ? 'w-screen h-screen absolute top-0 left-0 bg-black/40' : 'hidden'}></div>
 
-      <div>
-          {/* Overlay */}
-          <div className='w-screen h-screen absolute top-0 left-0 bg-black/40'></div>
-
-          <div className='flex flex-col items-end absolute bg-white top-0 right-0 w-[40%] h-screen'>
-            <AiOutlineClose size={25}/>
-            <ul className='flex flex-col justify-around items-center h-[200px]'>
-              <a className='hover:underline hover:underline-offset-3' href='#about'><li>About</li></a>
-              <a className='hover:underline hover:underline-offset-3' href='#matches'><li>Matches</li></a>
-              <a className='hover:underline hover:underline-offset-3' href='#teams'><li>Teams</li></a>
-              <a className='hover:underline hover:underline-offset-3' href='#more'><li>More</li></a>
-            </ul>
-          </div>
+        <div className={nav ? 'flex flex-col items-end absolute bg-gray-200 top-0 right-0 w-[200px] sm:w-[300px] p-4 h-screen' : 'hidden'}>
+          <AiOutlineClose onClick={() => setNav(!nav)} className='cursor-pointer' size={25}/>
+          <ul className='flex flex-col justify-around items-center h-[200px] mt-10'>
+            <a className='hover:underline hover:underline-offset-3' href='#about'><li>About</li></a>
+            <a className='hover:underline hover:underline-offset-3' href='#matches'><li>Matches</li></a>
+            <a className='hover:underline hover:underline-offset-3' href='#teams'><li>Teams</li></a>
+            <a className='hover:underline hover:underline-offset-3' href='#more'><li>More</li></a>
+          </ul>
         </div>
+
 
     </div>
   )
