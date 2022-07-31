e0317e0da54c23631e252d225542c17be77e9d29
milessteamaccount <ronaldlamdev@gmail.com>
2022-07-31 01:01:52 -0500

    began creating swiss round data

---
+++ src/assets/data/SwissRounds.js
@@ -0,0 +1,92 @@
+const swiss = [
+
+  // Round 1
+  {
+    id: 1,
+    map: 'Haunted Forest',
+    matches: {
+      game1: {
+        title: 'Lost in the Sauce vs. Duplex',
+        video: 'https://www.youtube.com/watch?v=SZwrNCTnjFQ'
+      },
+      game2: {
+        title: 'LuckyGlock vs Officially-Unofficial',
+        video: 'https://www.youtube.com/watch?v=X06OUh0xks0'
+      },
+      game3: {
+        title: 'Aquilax vs. Mix',
+        video: 'https://www.youtube.com/watch?v=fqzfKH_REW8'
+      },
+      game4: {
+        title: 'Frag4 vs Provid 19',
+        video: 'https://www.youtube.com/watch?v=1i4Ke-PwxNg'
+      },
+      game5: {
+        title: 'South Stars vs Penguins of Madagascar',
+        video: 'https://www.youtube.com/watch?v=9Rl8K1ASWqU'
+      },
+      game6: {
+        title: 'EVO vs. Constant Motion',
+        video: 'https://www.youtube.com/watch?v=NJtFWsyp8qQ'
+      }
+    } 
+  },
+
+  // Round 2 
+
+  {
+    id: 2,
+    map: 'Dead Air',
+    matches: {
+      game1: {
+        title: 'Frag4 vs CyberBullies',
+        video: 'https://www.youtube.com/watch?v=wycg0oE_HoM&t=3979s'
+      },
+      game2: {
+        title: 'Yung Bloods vs Quick Cleans',
+        video: 'https://www.youtube.com/watch?v=huYxc8XsDZY'
+      },
+      game3: {
+        title: 'LuckyGlock vs 44 Biceps',
+        video: 'https://www.twitch.tv/videos/628997121?filter=highlights&sort=time'
+      },
+      game4: {
+        title: 'Penguins of Madagascar vs. Nightmare Guns',
+        video: 'https://www.twitch.tv/videos/630477277?filter=highlights&sort=time'
+      },
+      game5: {
+        title: 'Aquilax vs AD',
+        video: 'https://www.youtube.com/watch?v=xMrw-YtM9RI'
+      },
+      game6: {
+        title: 'South Stars vs Deeply Closeted',
+        video: 'https://www.twitch.tv/videos/631345419?collection=c6MeofFsDxZOFg&filter=collections&sort=time'
+      }
+    }
+  },
+
+    // Round 3
+
+    {
+      id: 3,
+      map: 'Dead Center',
+      matches: {
+        game1: {
+          title: 'Frag4 vs LuckyGlock',
+          video: 'https://www.youtube.com/watch?v=vhfUwOYExeE&t=1015s'
+        },
+        game2: {
+          title: 'G2 vs Aquilax',
+          video: 'https://www.twitch.tv/videos/637281819?collection=c6MeofFsDxZOFg&filter=collections&sort=time'
+        },
+        game3: {
+          title: 'Five Non-Blakes vs. Constant Motion',
+          video: 'https://www.twitch.tv/videos/637682179?collection=c6MeofFsDxZOFg&filter=collections&sort=time'
+        },
+        game4: {
+          title: 'Vandalas vs Baller Squad',
+          video: 'https://www.twitch.tv/videos/636167355?collection=c6MeofFsDxZOFg&filter=collections&sort=time'
+        }
+      }
+    }
+]
