8aacea001b6f86eb7004e5eee69bff1fe65d5da5
milessteamaccount <ronaldlamdev@gmail.com>
2022-07-30 23:41:13 -0500

    finished adding text coloring to specific words in about

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -4,19 +4,21 @@ const About = () => {
   return (
     <section id='about' className='mt-16 mx-auto max-w-[400px] sm:max-w-[600px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1200px] 2xl:max-w-[1440px]'>
       <div className='flex flex-col lg:flex-row justify-around gap-8 text-center'>
-        <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border'>
+        <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border leading-7'>
           The <span className='text-[#FFF9D7] font-bold'>Quarantine Cup</span> of <span className='text-red-700 font-bold'>2020</span> is an international competitive Left 4 Dead 2 tournament 
           that kicked off in April 16, 2020 and concluded in August 21, 2020, with European Team <span className='font-semibold text-[#BD4291]'>Frag4</span> as the <span className='text-[#FFEF82]'>champions </span>
-            and North American Team <span className='text-[#EFD345]'>LuckyGlock</span> as the <span className='text-[#A0BCC2]'>runner-up</span>.  
+            and North American Team <span className='text-[#EFD345]'>LuckyGlock</span> as the <span className='text-[#A2B5BB]'>runner-up</span>.  
         </p>
-        <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border'>
-          The tournament was born out of the early periods of the global COVID epidemic back in March 2020. Many Left 4 Dead players found themselves
-          at home more frequently due to the nationwide sanctioned lockdown, therefore giving players more time to scrim (a gaming term for practicing 
-          competitive games). For this reason, it is how the Quarantine Cup of 2020 became the biggest tourney since the BleedOut Tournament of 2016.
+        <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border leading-7'>
+          The tournament was born out of the early periods of the global <span className='text-red-800 font-bold'>COVID</span> epidemic back in March 2020. Many Left 4 Dead players found themselves
+          at home more frequently due to the nationwide sanctioned lockdown, therefore giving players more time to <span className='text-[#53BF9D]'>scrim</span> (a gaming term for practicing 
+          competitive games). For this reason, it is how the <span className='text-[#FFF9D7] font-bold'>Quarantine Cup</span> of <span className='text-red-700 font-bold'>2020</span> became the 
+          biggest tourney since the <span className='text-[#990000] font-bold'>BleedOut Tournament</span> back in 2016.
         </p>
-        <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border'>
-          This community-hosted event comprised of 38 teams from different parts of the world. 
-          Each team can have 4 to 6 members, so the tournament spanned between 152 to 228 players competing for a cash prize of $500.
+        <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border leading-7'>
+          This community-hosted event comprised of <span className='text-[#F77E21]'>38 teams</span> from different parts of the world. 
+          Each team can have <span className='text-[#37E2D5]'>4 to 6</span> members, so the tournament spanned between <span className='text-[#37E2D5]'>152 to 228</span> 
+          players competing for a cash prize of <span className='text-[#5FD068]'>$500</span>.
         </p>
       </div>
     </section>
