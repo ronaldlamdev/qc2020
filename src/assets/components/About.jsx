fc08870b29c6006d4ab1d45bd43c367b8f6d6e0b
milessteamaccount <ronaldlamdev@gmail.com>
2022-08-03 20:10:00 -0500

    added new words, changed text color of swiss h2, copy and paste swiss matches html for playoffs html

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           and North American Team <span className='text-[#EFD345] font-semibold'>LuckyGlock</span> as the <span className='text-[#A2B5BB] font-bold'>runner-up</span>.  
         </p>
         <p className='text-white/80 bg-black/30 p-6 rounded-xl border-gray-100 border leading-7'>
-          The tournament was born out of the early periods of the global <span className='text-red-800 font-bold'>COVID</span> epidemic back in March 2020. Many Left 4 Dead players found themselves
+          The tournament was born out of the early periods of the global <span className='text-red-800 font-bold'>COVID-19</span> epidemic back in March 2020. Many Left 4 Dead 2 players found themselves
           at home more frequently due to the nationwide sanctioned lockdown, therefore giving players more time to <span className='text-[#53BF9D]'>scrim</span> (a gaming term for practicing 
           competitive games). For this reason, it is how the <span className='text-[#FFF9D7] font-bold'>Quarantine Cup</span> of <span className='text-red-700 font-bold'>2020</span> became the 
           biggest tourney since the <span className='text-[#990000] font-bold'>BleedOut Tournament</span> back in 2016.
@@ -24,7 +24,7 @@ const About = () => {
           This community-hosted event was comprised of <span className='text-[#F77E21]'><a className='hover:underline hover:underline-offset-3' target="_blank" href={teamsSteam}>38 teams</a></span> from different parts of the world. 
           Each team can have <span className='text-[#37E2D5]'>4 to 6</span> members, resulting the tournament player count between <span className='text-[#37E2D5]'>152 to 228 </span> 
           players competing for a cash prize of <span className='text-[#5FD068]'>$500</span>. The tournament is also divided into 2 stages: a <span className='text-[#F77E21]'>
-          6-round <a className='hover:underline hover:underline-offset-3' target="_blank" href={swissWiki}>Swiss Stage</a></span> and a <span className='text-[#FF0063]'>
+          6-round <a className='hover:underline hover:underline-offset-3' target="_blank" href={swissWiki}>Swiss Stage</a></span> and then a <span className='text-[#FF0063]'>
           Double Elimination <a className='hover:underline hover:underline-offset-3' target="_blank" href={playoffsWiki}> Playoffs Stage</a>
           </span>.
         </p>
--- src/assets/components/Matches.jsx
+++ src/assets/components/Matches.jsx
@@ -21,7 +21,7 @@ const Matches = () => {
 
       {/* Filter by Round */}
       <div className='max-w-[1640px]'>
-        <h2 className='text-center mt-20 mb-5 text-[#D61C4E] font-semibold text-2xl'>Swiss Stage</h2>
+        <h2 className='text-center mt-20 mb-5 text-[#F77E21] font-semibold text-2xl'>Swiss Stage</h2>
         <div className='flex flex-wrap gap-1 justify-around max-w-[1440px] p-4 mx-auto'>
           <button onClick={() => setSwissGames(swiss)} className='bg-[#1F4690] px-7 py-3 rounded-2xl text-white hover:bg-transparent hover:text-[#5E5DF0] border border-white duration-300 tracking-wider'>All</button>
           <button onClick={() => filterRound(1)} className='bg-[#1F4690] px-2 py-3 rounded-2xl text-white hover:bg-transparent hover:text-[#5E5DF0] border border-white duration-300'>Round 1</button>
@@ -33,6 +33,8 @@ const Matches = () => {
         </div>
       </div>
 
+      {/* Swiss Stage Matches */}
+
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10'>
         {games.map((game, index) => (
           <div className='flex justify-center flex-col' key={index}>
@@ -43,6 +45,19 @@ const Matches = () => {
           </div>
         ))}
       </div>
+
+      {/* Playoffs Stage Matches */}
+
+      {/* <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-10'>
+        {games.map((game, index) => (
+          <div className='flex justify-center flex-col' key={index}>
+            <iframe className='w-full h-[300px] rounded-md mb-2' src={game.video}></iframe>
+            <div className="flex justify-center bg-black/30 border border-white rounded-lg">
+              <p className='text-white py-4'><span className='text-[#72FFFF] font-bold'>{game.team1}</span> vs. <span className='text-red-600 font-bold'>{game.team2}</span></p>
+            </div>
+          </div>
+        ))}
+      </div> */}
     </div>
   )
 }
